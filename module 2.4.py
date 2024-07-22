9284f1e3498ac1b35fcc44f9655e5f0db1aff0c7
9494Guzel <guzel.galiulina.94@inbox.ru>
2024-07-22 19:14:31 +0300

    Add files via upload

---
+++ module 2.4.py
@@ -0,0 +1,14 @@
+numbers=[1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes=[2, 3, 5, 7, 9, 11, 13, 15]
+not_primes=[4, 6, 8, 10, 12, 14]
+print(primes)
+print(not_primes)
+for i in primes:
+    print('ok')
+    for i in not_primes:
+        print('me')
+for i in primes:
+    print(i)
+for i in not_primes:
+    print(i)
+numbers(True)
